9d805d2288aa58301d9a576f87b59e60ffb30794
Simone Basso <bassosimone@gmail.com>
2016-07-08 02:18:28 +0200

    Let the module compile with Python3

--- py_module.cpp
+++ py_module.cpp
@@ -121,12 +121,32 @@ static PyMethodDef Methods[] = {
     {nullptr, nullptr, 0, nullptr},
 };
 
-PyMODINIT_FUNC init_mk() {
-    PyObject *module = Py_InitModule("_mk", Methods);
+// http://python3porting.com/cextensions.html
+#if PY_MAJOR_VERSION >= 3
+  #define MOD_ERROR_VAL NULL
+  #define MOD_SUCCESS_VAL(val) val
+  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          static struct PyModuleDef moduledef = { \
+            PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+          ob = PyModule_Create(&moduledef);
+#else
+  #define MOD_ERROR_VAL
+  #define MOD_SUCCESS_VAL(val)
+  #define MOD_INIT(name) void init##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          ob = Py_InitModule3(name, methods, doc);
+#endif
+
+MOD_INIT(_mk) {
+    PyObject *module = nullptr;
+
+    MOD_DEF(module, "_mk", "MeasurementKit bindings", Methods);
     if (module == nullptr) {
-        return;
+        return MOD_ERROR_VAL;
     }
     PyEval_InitThreads(); // Tell Python we're going to use threads
+    return MOD_SUCCESS_VAL(module);
 }
 
 }
