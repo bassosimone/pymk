9283e2c969e06e59dc8452a9979f0947004c46d0
Simone Basso <bassosimone@gmail.com>
2016-08-01 16:10:12 +0200

    Add Python wrapper

---
+++ measurement_kit.py
@@ -0,0 +1,81 @@
+# Part of measurement-kit <https://measurement-kit.github.io/>.
+# Measurement-kit is free software. See AUTHORS and LICENSE for more
+# information on the copying conditions.
+
+from __future__ import print_function
+
+import _measurement_kit as _mk
+
+# Note: keep this in sync with <measurement_kit/logger.hpp>
+MK_LOG_WARNING = 0
+MK_LOG_INFO = 1
+MK_LOG_DEBUG = 2
+MK_LOG_DEBUG2 = 3
+MK_LOG_VERBOSITY_MASK = 31
+MK_LOG_JSON = 32
+
+# TODO: make sure that the version matches
+
+
+class _BaseTest(object):
+
+    def __init__(self, name):
+        self._handle = _mk.create(name)
+
+    def __del__(self):
+        _mk.destroy(self._handle)
+
+    def set_verbosity(self, value):
+        _mk.set_verbosity(self._handle, value)
+        return self
+
+    def increase_verbosity(self):
+        _mk.increase_verbosity(self._handle)
+        return self
+
+    def on_log(self, callback):
+        _mk.on_log(self._handle, callback)
+        return self
+
+    def set_input_filepath(self, value):
+        _mk.set_input_filepath(self._handle, value)
+        return self
+
+    def set_output_filepath(self, value):
+        _mk.set_output_filepath(self._handle, value)
+        return self
+
+    def set_options(self, key, value):
+        _mk.set_options(self._handle, key, value)
+        return self
+
+    def run_async(self, callback):
+        _mk.run_async(self._handle, callback)
+
+    def run_deferred(self):
+        from twisted import defer
+        d = defer.Deferred()
+        def callback():
+            d.callback()
+        _mk.run_async(self._handle, callback)
+        return d
+
+
+class DnsInjection(_BaseTest):
+    def __init__(self):
+        super(self.__class__, self).__init__(b"dns_injection")
+
+
+class HttpInvalidRequestLine(_BaseTest):
+    def __init__(self):
+        super(self.__class__, self).__init__(b"http_invalid_request_line")
+
+
+class NdtTest(_BaseTest):
+    def __init__(self):
+        super(self.__class__, self).__init__(b"ndt")
+
+
+class TcpConnect(_BaseTest):
+    def __init__(self, easy):
+        super(self.__class__, self).__init__(b"ooni/tcp_connect")
--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@
 
 from distutils.core import setup, Extension
 
-module1 = Extension('_measurement_kit',
-                    language = "c++",
-                    extra_compile_args = ['-std=c++11'],
-                    libraries = ['measurement_kit'],
-                    sources = ['py_module.cpp'])
+extension = Extension('_measurement_kit',
+                      language = "c++",
+                      extra_compile_args = ['-std=c++11'],
+                      libraries = ['measurement_kit'],
+                      sources = ['py_module.cpp'])
 
-setup (name = 'measurement_kit',
-       version = '1.0',
-       description = 'Portable C++11 network measurement library',
-       author = 'Simone Basso',
-       author_email = 'bassosimone@gmail.com',
-       url = 'https://measurement-kit.github.io/',
-       long_description = '''
+setup(name = 'measurement_kit',
+      version = '0.3.0-dev.1',
+      description = 'Portable C++11 network measurement library',
+      author = 'Simone Basso',
+      author_email = 'bassosimone@gmail.com',
+      url = 'https://measurement-kit.github.io/',
+      long_description = '''
 Portable C++11 network measurement library (Python bindings)
 ''',
-       ext_modules = [module1])
+      py_modules = ["measurement_kit"],
+      ext_modules = [extension]
+      )
