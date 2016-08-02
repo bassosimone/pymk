d136c2a5afc3563ed765bd5b6c1d2eb272f2a5c6
Simone Basso <bassosimone@gmail.com>
2016-08-02 15:32:37 +0200

    examples: cosmetic changes

--- examples/web_connectivity.py
+++ examples/web_connectivity.py
@@ -2,7 +2,11 @@
 # Measurement-kit is free software. See AUTHORS and LICENSE for more
 # information on the copying conditions.
 
+""" Example showing how to run OONI's WebConnectivity test
+    using MeasurementKit and Twisted """
+
 from __future__ import print_function
+
 import measurement_kit
 from twisted.internet import reactor
 
@@ -13,14 +17,14 @@ def main():
         """ Print the entry when it is ready """
         print(entry)
 
-    d = measurement_kit.WebConnectivity()                                      \
+    done = measurement_kit.WebConnectivity()                                   \
         .set_verbosity(measurement_kit.MK_LOG_INFO)                            \
         .set_options(b"nameserver", b"8.8.8.8:53")                             \
         .set_input_filepath(b"fixtures/urls.txt")                              \
         .on_entry(print_entry)                                                 \
         .run_deferred()
 
-    d.addCallback(lambda *args: reactor.callFromThread(reactor.stop))
+    done.addCallback(lambda *args: reactor.callFromThread(reactor.stop))
     reactor.run()
 
 if __name__ == "__main__":
